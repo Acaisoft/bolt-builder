ae1d6b84ba4bce1bfee0d56bbccea282a3b759d9
art.barysevich <art.barysevich@gmail.com>
2019-06-27 13:44:57 +0200

    Save logs to database about preparation steps

--- builder/build.py
+++ builder/build.py
@@ -1,15 +1,16 @@
 import tempfile
 import uuid
 import os
-
+import logging
+import sys
 import git
+
+from cache import Cache
 
 from google_cloud_build import GoogleCloudBuild
 from locust_wrapper_packer import LocustWrapperPacker
-from cache import Cache
+from execution_stage_log import send_stage_log
 
-import logging
-import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
@@ -37,16 +38,21 @@ repo_path = tempfile.mkdtemp()
 tenant_id = os.environ.get('TENANT_ID')
 project_id = os.environ.get('PROJECT_ID')
 
+send_stage_log('Starting preparation')
 logger.info(f'Cloning repository {repo_url}...')
+send_stage_log('Cloning repository...')
 repo = git.Repo.clone_from(repo_url, repo_path, depth=1)
 logger.info(f'Repository cloned to {repo_path}')
+send_stage_log('Repository cloned')
 head_sha = repo.head.object.hexsha
 
 if not int(os.environ.get('NO_CACHE')):
     docker_image = cache.get_docker_image_by_sha(tenant_id, project_id, head_sha)
     if docker_image:
         logger.info(f'Found image in cache: {docker_image}')
+        send_stage_log('Got image from cache')
         write_output(docker_image)
+        send_stage_log('Preparation finished')
         exit(0)
 
 logger.info('Wrapping repository')
@@ -56,11 +62,14 @@ logger.info('Repository wrapped')
 
 destination = get_docker_image_destination(tenant_id, project_id)
 logger.info(f'Starting to build image {destination}')
+send_stage_log('Building new image...')
 google_cloud_build = GoogleCloudBuild()
 google_cloud_build.build(repo_path, destination)
 logger.info('Image built')
+send_stage_log('Image built')
 
 cache.set_docker_image_by_sha(tenant_id, project_id, head_sha, destination)
 logger.info(f'Written image to cache: {destination}')
 
 write_output(destination)
+send_stage_log('Preparation finished')
---
+++ builder/execution_stage_log.py
@@ -0,0 +1,32 @@
+import os
+import datetime
+import requests
+
+
+GRAPHQL_URL = os.environ['BOLT_GRAPHQL_URL']
+EXECUTION_ID = os.environ['BOLT_EXECUTION_ID']
+HASURA_TOKEN = os.environ['BOLT_HASURA_TOKEN']
+
+
+def send_stage_log(message, level: str = 'info'):
+    ts = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    query = '''
+    mutation ($log_entry:execution_stage_log_insert_input!) {
+        insert_execution_stage_log(objects:[$log_entry]) { affected_rows }
+    }
+    '''
+    body = {
+        'query': query,
+        'variables': {
+            'log_entry': {
+                'timestamp': ts,
+                'stage': 'argo_preparation',
+                'level': level,
+                'msg': message,
+            }
+        }
+    }
+    headers = {'Authorization': f'Bearer {HASURA_TOKEN}'}
+    response = requests.post(GRAPHQL_URL, json=body, headers=headers)
+    assert response.json().get('data') == {"insert_execution_stage_log": {"affected_rows": 1}}
+    response.raise_for_status()
